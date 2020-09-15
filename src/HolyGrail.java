f3ba95570f39195aefcd26bf84cb03435534a109
22ridley <22ridley@da.org>
2020-09-15 10:49:35 -0400

    Second commit with edits

--- src/HolyGrail.java
+++ src/HolyGrail.java
@@ -1,27 +1,35 @@
+/*
+primitives (holdover from C/C++):
+int (32 bits), double (256 bits), boolean, char
+Objects:
+- Starts with a capital letter
+- Every string is an object, a scanner is an object
+- Every object is an object
+ */
+
+// this imports a library when I compile this file
+// java language, util package, scanner class
+import java.util.Scanner;
 /**
  * HolyGrail.java is the first lab I will write
- * it is a simple tUI program that talks to the user.
+ * it is a simple UI program that talks to the user.
  * 09/15/2020
  * @author 22ridley
  */
 
-
-import java.util.Scanner;
-
 public class HolyGrail {
     public static void main(String[] args){
-        Scanner scanner = new Scanner( System.in );
+        // the jvm assigns the memory address when you use 'new'
+        Scanner scanner = new Scanner(System.in);
         System.out.println("* A chat with the bridge keeper *");
-        System.out.println("Who would cross the Bridge of Death must answer me these\n" +
-                "questions three, ere the other side he see.");
+        System.out.println("Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.");
         System.out.print( "Question 1: What is your name? " );
         String name = scanner.nextLine();
         System.out.print( "Question 2: What is your quest? " );
         String quest = scanner.nextLine();
         System.out.print( "Question 3: What is your favorite color? " );
         String color = scanner.nextLine();
-        System.out.println("King Arthur says, \"You have to know these things when you're a\n" +
-                "king, you know.\"");
+        System.out.println("King Arthur says, \"You have to know these things when you're a king, you know.\"");
         System.out.println("Your name is: " + name);
         System.out.println("Your quest is: " + quest);
         System.out.println("Your favorite color is: " + color);
